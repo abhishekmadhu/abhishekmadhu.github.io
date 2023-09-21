c39fbdd08ba7eaa93533427a4192ea32881deb16
Abhishek Madhu <21152008+abhishekmadhu@users.noreply.github.com>
2023-09-22 05:14:51 +0530

    Change image.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var h1 = document.querySelector("h1");
 var isBlue = false;
-setInterval(function(){
-    if (isBlue){
-        h1.style.color = "white";
+setInterval(function () {
+    if (isBlue) {
+        h1.style.color = "black";
     } else {
-        h1.style.color = "#3700b3";
+        h1.style.color = "#2c0084";
     }
     isBlue = !isBlue;
 }, 1000);
